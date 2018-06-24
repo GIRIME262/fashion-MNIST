da5d42411fbe4ee282966bce05d1ad44e95f56df
Julian Ganzabal <jganzabal@gmail.com>
2018-06-24 14:54:29 -0300

    updates

--- cnn_models.py
+++ cnn_models.py
@@ -116,7 +116,7 @@ def get_model_4():
     model3.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model3
 
-def get_model_5(p=0.2):
+def get_model_5(pd=0.3, pc=0.2):
     model3=Sequential()
     model3.add(BatchNormalization(input_shape=(28,28,1)))
     model3.add(Conv2D(filters=6, kernel_size=(3, 3), padding="same", name='Conv1',
@@ -127,7 +127,7 @@ def get_model_5(p=0.2):
     model3.add(BatchNormalization())
     model3.add(Activation('relu'))
     model3.add(MaxPooling2D(pool_size=(2, 2), name='MaxPool1'))
-    model3.add(Dropout(p))
+    model3.add(Dropout(pc))
     model3.add(Conv2D(filters=24, kernel_size=(3, 3), padding="same", name='Conv3'))
     model3.add(BatchNormalization())
     model3.add(Activation('relu'))
@@ -135,13 +135,14 @@ def get_model_5(p=0.2):
     model3.add(BatchNormalization())
     model3.add(Activation('relu'))
     model3.add(MaxPooling2D(pool_size=(3, 3), name='MaxPool2'))
-    model3.add(Dropout(p))
+    model3.add(Dropout(pc))
     model3.add(Flatten())
     model3.add(Dense(256))
     model3.add(LeakyReLU())
-    model3.add(Dropout(p))
+    model3.add(Dropout(pd))
     model3.add(Dense(256))
     model3.add(LeakyReLU())
+    model3.add(Dropout(pd))
     model3.add(Dense(10, activation='softmax'))
     model3.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model3
